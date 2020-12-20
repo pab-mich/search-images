531e6f00d9b1e8949640d928550c3fcd9ad44c6f
Pablo Agustin Michelini <pab.michelini@gmail.comgit config --global user.name pab$ git config --global user.email pab.michelini@gmail.com>
2020-12-19 21:20:54 -0300

    first comit

---
+++ src/components/found-image.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./home.css";
+import{FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import{faCamera} from '@fortawesome/free-solid-svg-icons';
+
+
+function FoundImage(){
+
+    return(
+        <>
+        
+            <div className="bg">
+               <div className="container">
+             <div className="col-md-12">
+
+             <div className="text-center ">
+           
+           
+            <FontAwesomeIcon className="camera centrado " icon= {faCamera}/>
+            <h1>Search Photos</h1>
+            </div>
+            </div> 
+            </div>
+            </div>
+        </>
+        
+         )
+}
+
+export default FoundImage;
---
+++ src/components/images.js
@@ -0,0 +1,62 @@
+import React from "react";
+import axios from 'axios';
+import "./home.css";
+import FoundImage from '../components/found-image.js';
+import {useState} from "react";
+
+function Images(){
+
+    const [photo,setPhoto] = useState("");
+    const [clientId, setClientId] = useState("SK8B8o5MLRvek2wongv5lmtZ7ZopOb0_oTYvI0h5Jt8");
+
+    const [result,setResult] = useState([])
+
+    function handleCahnge(event){
+        setPhoto(event.target.value);
+    }
+
+    function handleSubmit(event){
+        console.log(photo);
+        
+        const url= "https://api.unsplash.com/search/photos?query="+photo+"&client_id="+clientId;
+
+        axios.get(url).then( response =>{
+           console.log(response);
+           setResult(response.data.results); 
+        })
+    }
+
+
+    return (
+        <>
+       <FoundImage/>
+        <div className="container">
+      
+        <div className="row centrado">
+       
+        <input onChange={handleCahnge} type="text" name="photo" placeholder="Beatifull Photos" className="busqueda" id="miid" />
+        
+        <div className="actions">
+        <button onClick={handleSubmit} type="submit" className="btng" >Find</button>
+        </div>
+        </div>
+        </div>
+        <div className="col-md-12 images">
+     
+        {result.map((photo) => (
+         
+        <img className="pic" src={photo.urls.small}/>
+        
+
+        ))}
+        
+        </div>
+        
+       
+        </>
+
+    )
+}
+
+export default Images;
+
